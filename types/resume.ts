0b820cee5eddab512fafc201c4c7f64baadf175f
Andrea Olivieri <an.olivieri@reply.it>
2025-11-30 12:54:52 +0100

    docs: Remove technology stacks from project descriptions in resume data.

--- types/resume.ts
+++ types/resume.ts
@@ -21,6 +21,7 @@ export interface ResumeData {
     logoText?: string;
     logo?: string;
     description: string;
+    techStack?: string[];
     projects?: Array<{
       company: string;
       location?: string;
@@ -30,6 +31,7 @@ export interface ResumeData {
       logoText?: string;
       logo?: string;
       description: string;
+      techStack?: string[];
     }>;
   }>;
   education: Array<{
